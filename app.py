2ccce0a0938a68ca0f19067c7f93a1d4208d9f7a
Zhiyu(Garros) Gong <66699813+garroshub@users.noreply.github.com>
2022-11-19 19:51:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,243 +1,88 @@
-import timeit
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import warnings
-warnings.filterwarnings("ignore")
-
 import streamlit as st
-st.title('Credit Card Fraud Detection!')
+import json
+import requests as re
 
-df=st.cache(pd.read_csv)('creditcard.csv')
+st.title("Credit Card Fraud Detection Web App")
 
-#df = df.sample(frac=0.1, random_state = 48)
+st.image("image.png")
 
-# Print shape and description of the data
-if st.sidebar.checkbox('Show what the dataframe looks like'):
-    st.write(df.head(100))
-    st.write('Shape of the dataframe: ',df.shape)
-    st.write('Data decription: \n',df.describe())
+st.write("""
+## About
+Credit card fraud is a form of identity theft that involves an unauthorized taking of another's credit card information for the purpose of charging purchases to the account or removing funds from it.
+
+**This Streamlit App utilizes a Machine Learning model served as an API in order to detect fraudulent credit card transactions based on the following criteria: hours, type of transaction, amount, balance before and after transaction etc.** 
+
+""")
 
 
-# Print valid and fraud transactions
-fraud=df[df.Class==1]
-valid=df[df.Class==0]
-outlier_percentage=(df.Class.value_counts()[1]/df.Class.value_counts()[0])*100
+st.sidebar.header('Input Features of The Transaction')
 
-if st.sidebar.checkbox('Show fraud and valid transaction details'):
-    st.write('Fraudulent transactions are: %.3f%%'%outlier_percentage)
-    st.write('Fraud Cases: ',len(fraud))
-    st.write('Valid Cases: ',len(valid))
-
+sender_name = st.sidebar.text_input("""Input Sender ID""")
+receiver_name = st.sidebar.text_input("""Input Receiver ID""")
+step = st.sidebar.slider("""Number of Hours it took the Transaction to complete: """)
+types = st.sidebar.subheader(f"""
+                 Enter Type of Transfer Made:\n\n\n\n
+                 0 for 'Cash In' Transaction\n 
+                 1 for 'Cash Out' Transaction\n 
+                 2 for 'Debit' Transaction\n
+                 3 for 'Payment' Transaction\n  
+                 4 for 'Transfer' Transaction\n""")
+types = st.sidebar.selectbox("",(0,1,2,3,4))
+x = ''
+if types == 0:
+    x = 'Cash in'
+if types == 1:
+    x = 'Cash Out'
+if types == 2:
+    x = 'Debit'
+if types == 3:
+    x = 'Payment'
+if types == 4:
+    x =  'Transfer'
     
-#Obtaining X (features) and y (labels)
-X=df.drop(['Class'], axis=1)
-y=df.Class
+amount = st.sidebar.number_input("Amount in $",min_value=0, max_value=110000)
+oldbalanceorg = st.sidebar.number_input("""Sender Balance Before Transaction was made""",min_value=0, max_value=110000)
+newbalanceorg= st.sidebar.number_input("""Sender Balance After Transaction was made""",min_value=0, max_value=110000)
+oldbalancedest= st.sidebar.number_input("""Recipient Balance Before Transaction was made""",min_value=0, max_value=110000)
+newbalancedest= st.sidebar.number_input("""Recipient Balance After Transaction was made""",min_value=0, max_value=110000)
+isflaggedfraud = 0
+if amount >= 200000:
+  isflaggedfraud = 1
+else:
+  isflaggedfraud = 0
 
-# Split the data into training and testing sets
-from sklearn.model_selection import train_test_split
-size = st.sidebar.slider('Test Set Size', min_value=0.2, max_value=0.4)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = size, random_state = 42)
+if st.button("Detection Result"):
+    values = {
+    "step": step,
+    "types": types,
+    "amount": amount,
+    "oldbalanceorig": oldbalanceorg,
+    "newbalanceorig": newbalanceorg,
+    "oldbalancedest": oldbalancedest,
+    "newbalancedest": newbalancedest,
+    "isflaggedfraud": isflaggedfraud
+    }
 
-#Print shape of train and test sets
-if st.sidebar.checkbox('Show the shape of training and test set features and labels'):
-    st.write('X_train: ',X_train.shape)
-    st.write('y_train: ',y_train.shape)
-    st.write('X_test: ',X_test.shape)
-    st.write('y_test: ',y_test.shape)
+
+    st.write(f"""### These are the transaction details:\n
+    Sender ID: {sender_name}
+    Receiver ID: {receiver_name}
+    1. Number of Hours it took to complete: {step}\n
+    2. Type of Transaction: {x}\n
+    3. Amount Sent: {amount}$\n
+    4. Sender Balance Before Transaction: {oldbalanceorg}$\n
+    5. Sender Balance After Transaction: {newbalanceorg}$\n
+    6. Recepient Balance Before Transaction: {oldbalancedest}$\n
+    7. Recepient Balance After Transaction: {newbalancedest}$\n
+    8. System Flag Fraud Status(Transaction amount greater than $200000): {isflaggedfraud}
+                """)
+
+    res = re.post(f"https://credit-fraud-ml-api.herokuapp.com/predict",json=values)
+    json_str = json.dumps(res.json())
+    resp = json.loads(json_str)
     
-
-#Import classification models and metrics
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
-from sklearn.model_selection import cross_val_score
-
-
-logreg=LogisticRegression()
-svm=SVC()
-knn=KNeighborsClassifier()
-etree=ExtraTreesClassifier(random_state=42)
-rforest=RandomForestClassifier(random_state=42)
-
-
-features=X_train.columns.tolist()
-
-
-#Feature selection through feature importance
-@st.cache
-def feature_sort(model,X_train,y_train):
-    #feature selection
-    mod=model
-    # fit the model
-    mod.fit(X_train, y_train)
-    # get importance
-    imp = mod.feature_importances_
-    return imp
-
-#Classifiers for feature importance
-clf=['Extra Trees','Random Forest']
-mod_feature = st.sidebar.selectbox('Which model for feature importance?', clf)
-
-start_time = timeit.default_timer()
-if mod_feature=='Extra Trees':
-    model=etree
-    importance=feature_sort(model,X_train,y_train)
-elif mod_feature=='Random Forest':
-    model=rforest
-    importance=feature_sort(model,X_train,y_train)
-elapsed = timeit.default_timer() - start_time
-st.write('Execution Time for feature selection: %.2f minutes'%(elapsed/60))    
-
-#Plot of feature importance
-if st.sidebar.checkbox('Show plot of feature importance'):
-    plt.bar([x for x in range(len(importance))], importance)
-    plt.title('Feature Importance')
-    plt.xlabel('Feature (Variable Number)')
-    plt.ylabel('Importance')
-    st.pyplot()
-
-feature_imp=list(zip(features,importance))
-feature_sort=sorted(feature_imp, key = lambda x: x[1])
-
-n_top_features = st.sidebar.slider('Number of top features', min_value=5, max_value=20)
-
-top_features=list(list(zip(*feature_sort[-n_top_features:]))[0])
-
-if st.sidebar.checkbox('Show selected top features'):
-    st.write('Top %d features in order of importance are: %s'%(n_top_features,top_features[::-1]))
-
-X_train_sfs=X_train[top_features]
-X_test_sfs=X_test[top_features]
-
-X_train_sfs_scaled=X_train_sfs
-X_test_sfs_scaled=X_test_sfs
-
-
-
-#Import performance metrics, imbalanced rectifiers
-from sklearn.metrics import  confusion_matrix,classification_report,matthews_corrcoef
-from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import NearMiss
-np.random.seed(42) #for reproducibility since SMOTE and Near Miss use randomizations
-
-smt = SMOTE()
-nr = NearMiss()
-
-def compute_performance(model, X_train, y_train,X_test,y_test):
-    start_time = timeit.default_timer()
-    scores = cross_val_score(model, X_train, y_train, cv=3, scoring='accuracy').mean()
-    'Accuracy: ',scores
-    model.fit(X_train,y_train)
-    y_pred = model.predict(X_test)
-    cm=confusion_matrix(y_test,y_pred)
-    'Confusion Matrix: ',cm  
-    cr=classification_report(y_test, y_pred)
-    'Classification Report: ',cr
-    mcc= matthews_corrcoef(y_test, y_pred)
-    'Matthews Correlation Coefficient: ',mcc
-    elapsed = timeit.default_timer() - start_time
-    'Execution Time for performance computation: %.2f minutes'%(elapsed/60)
-    
-
-#Run different classification models with rectifiers
-if st.sidebar.checkbox('Run a credit card fraud detection model'):
-    
-    alg=['Extra Trees','Random Forest','k Nearest Neighbor','Support Vector Machine','Logistic Regression']
-    classifier = st.sidebar.selectbox('Which algorithm?', alg)
-    rectifier=['SMOTE','Near Miss','No Rectifier']
-    imb_rect = st.sidebar.selectbox('Which imbalanced class rectifier?', rectifier) 
-    
-    if classifier=='Logistic Regression':
-        model=logreg
-        if imb_rect=='No Rectifier':
-            compute_performance(model, X_train_sfs_scaled, y_train,X_test_sfs_scaled,y_test)
-        elif imb_rect=='SMOTE':
-                rect=smt
-                st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-                X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-                st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-                compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-        elif imb_rect=='Near Miss':
-            rect=nr
-            st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-            X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-            st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-            compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-    
-        
-    elif classifier == 'k Nearest Neighbor':
-        model=knn
-        if imb_rect=='No Rectifier':
-            compute_performance(model, X_train_sfs_scaled, y_train,X_test_sfs_scaled,y_test)
-        elif imb_rect=='SMOTE':
-                rect=smt
-                st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-                X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-                st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-                compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-        elif imb_rect=='Near Miss':
-            rect=nr
-            st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-            X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-            st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-            compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)    
-    
-    elif classifier == 'Support Vector Machine':
-        model=svm
-        if imb_rect=='No Rectifier':
-            compute_performance(model, X_train_sfs_scaled, y_train,X_test_sfs_scaled,y_test)
-        elif imb_rect=='SMOTE':
-                rect=smt
-                st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-                X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-                st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-                compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-        elif imb_rect=='Near Miss':
-            rect=nr
-            st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-            X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-            st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-            compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)    
-        
-    elif classifier == 'Random Forest':
-        model=rforest
-        if imb_rect=='No Rectifier':
-            compute_performance(model, X_train_sfs_scaled, y_train,X_test_sfs_scaled,y_test)
-        elif imb_rect=='SMOTE':
-                rect=smt
-                st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-                X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-                st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-                compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-        elif imb_rect=='Near Miss':
-            rect=nr
-            st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-            X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-            st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-            compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)  
-            
-    elif classifier == 'Extra Trees':
-        model=etree
-        if imb_rect=='No Rectifier':
-            compute_performance(model, X_train_sfs_scaled, y_train,X_test_sfs_scaled,y_test)
-        elif imb_rect=='SMOTE':
-                rect=smt
-                st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-                X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-                st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-                compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-        elif imb_rect=='Near Miss':
-            rect=nr
-            st.write('Shape of imbalanced y_train: ',np.bincount(y_train))
-            X_train_bal, y_train_bal = rect.fit_sample(X_train_sfs_scaled, y_train)
-            st.write('Shape of balanced y_train: ',np.bincount(y_train_bal))
-            compute_performance(model, X_train_bal, y_train_bal,X_test_sfs_scaled,y_test)
-            
-    
-        
-
-
-
+    if sender_name=='' or receiver_name == '':
+        st.write("Error! Please input Transaction ID or Names of Sender and Receiver!")
+    else:
+        st.write(f"""### The '{x}' transaction that took place between {sender_name} and {receiver_name} is {resp[0]}.""")
